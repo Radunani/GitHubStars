dcbbbd4ddd623f04c75826e92297f4a9d9562945
Radunani <mrsantateam@yandex.ru>
2026-03-13 18:22:42 +0200

    Project

---
+++ GitHubStars/GitHubStarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubStarsApp.swift
+//  GitHubStars
+//
+//  Created by Radu Nani on 13.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct GitHubStarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
